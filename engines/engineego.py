b9614354d0c699cf8b291f7dbb2648dba7754b6a
stadiumphenom <33994205+stadiumphenom@users.noreply.github.com>
2025-07-03 10:20:29 -0700

    Add files via upload

---
+++ engines/engineego.py
@@ -0,0 +1,9 @@
+# engineego.py
+from engines.enginecore import MyriadBaseEngine
+
+class MyriadEgoEngine(MyriadBaseEngine):
+    def _generate_keywords(self):
+        return ["dissolve", "oneness", "lightwave", "transcend"]
+
+    def _calculate_intensity(self):
+        return round(0.75 + self.drift_factor * 2, 2)
