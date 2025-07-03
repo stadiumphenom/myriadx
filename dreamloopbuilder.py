80c81019d5ce58366bcce61cbb38a8c8406591c2
stadiumphenom <33994205+stadiumphenom@users.noreply.github.com>
2025-07-03 13:32:06 -0700

    Update dreamloopbuilder.py

--- dreamloopbuilder.py
+++ dreamloopbuilder.py
@@ -3,7 +3,7 @@
 
 import os
 import time
-from sd_to_myriad1 import analyze_image
+from sd_to_myriad import analyze_image
 from myriad_sd_bridge import buildsdprompt, exporttosd
 from engines import engine_ego, engine_echo, engine_drift, engine_form
 from myriad_sd_mock import simulate_sd_render
